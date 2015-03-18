bd151ad9c053f9d65ef958b6cc8a4bb73f090913
raufagayev <agayevrauf@gmail.com>
2015-03-18 22:03:19 +0100

    new features added to Modeler part

--- BuildingGenerator/BuildingGenerator/Files/Modeler/Modeler.cpp
+++ BuildingGenerator/BuildingGenerator/Files/Modeler/Modeler.cpp
@@ -29,17 +29,126 @@ namespace modl
 	{
 
 	}
+
+	vector<Symbol*>queue;
+
+	//Symbol* tree;
+	//ofstream cout("C:\\Users\\RaufA\\Desktop\\project pre\\project\\BuildingGenerator\\BuildingGenerator\\Files\\items.txt");
+	void Modeler::init(){	
+				//initialize root of Tree with parameters - name, position, scale
+			
+		        tree = new Symbol("start");
+				tree->setPosition(0.0, 0.0, 0.0);
+				tree->setScale(1.0,1.0,1.0);				
+				queue.push_back(tree);			
+				
+	}
+
+	void Modeler::printTree(){
+		
+				//after building derivation tree, print is being done, by starting from root node and using BFS logic
+
+				/*ofstream cout("C:\\Users\\RaufA\\Desktop\\project pre\\project\\BuildingGenerator\\BuildingGenerator\\Files\\output.txt");				
+
+				queue.push_back(tree);			
+				
+				cout << "------------------------------------------------" << endl;
+				while(queue.size()){
+					Symbol* cur_item = queue[0];				
+				    queue.erase(queue.begin());	
+					cout<<"Symbol Name: "<<cur_item->getSymbolName()<<endl;
+					Vector3 position = cur_item->getPosition();
+					cout<<"Position: ("<<position.x<<", "<<position.y<<", "<<position.z<<")"<<endl;
+					Vector3 scale = cur_item->getScale();
+					cout<<"Scale: ("<<scale.x<<", "<<scale.y<<", "<<scale.z<<")"<<endl;
+					cout<<"Children: { ";
+					for(int i = 0;i<cur_item->Children.size();i++){
+						Symbol *child = cur_item->Children[i];
+						cout<<child->getSymbolName()<<" ";
+						queue.push_back(child);
+					}
+					cout << "}"<<endl;
+				cout << "------------------------------------------------" << endl;
+					
+				}
+				
+				cout.close();*/
+	
+	}
+
+	double createRandom(){	
+			//generates random number between 0 and 1		
+			srand(time(NULL));
+			return ((double)(rand()%100)+1)/100.0;
+	}
+
 	void Modeler::Model(std::unordered_map < std::string, std::vector<comn::Rule>> ruleSet)
 	{
-		// uses the ruleSet to construct the deviation tree
+				/*
+				 By using BFS approach, each time one element is being selected and children are created from it.
+				 Those nodes are components of shape.
+				 */
+
+				double  probability = createRandom();	
+
+				int index = (probability<=0.40)?1:0;	
+		
+				init();		
+		
+				int pre_index = index;
+
+				while(queue.size()){				    
+					Symbol *cur_item = queue[0];				
+				    queue.erase(queue.begin());		
+					
+					string shape_name = cur_item->getSymbolName();
+					
+
+					if(ruleSet.find(shape_name)!=ruleSet.end()){  //searchs for shape name in given map
+					vector<Rule>rules = ruleSet.at(shape_name);
+					index = (rules.size()>1)?index:0;
+					string function_name = rules[index].functionName;		
+					
+					if(function_name.compare("comp")==0){						
+						comp(rules[index].childNames,cur_item);
+					}else if(function_name.compare("S")==0){						
+						S(rules[index].functionParameters, rules[index].childNames, cur_item);					
+					}else if(function_name.compare("repeat")==0){
+						repeat(rules[index].functionParameters, rules[index].childNames,cur_item);
+					}else if(function_name.compare("subDiv")==0){
+						subDiv(rules[index].functionParameters, rules[index].childNames,cur_item);
+					}else{
+						nullFunction(rules[index].childNames,cur_item);
+					}					
+					}
+					index = pre_index;
+			   }  			
+					printTree();
 	}
-	
+
+
 	comn::Symbol* Modeler::GenerateTestData()
 	{   
 		comn::Symbol* node = new Symbol();
 		return node;
 	}
 	
+	void Modeler::print(Symbol* item){
+		/*
+				cout<<item->getSymbolName()<<"-> ";
+				cout<<"Position: ";
+				Vector3 pos = item->getPosition();
+				for(int k = 0;k<pos.size();k++)cout<<pos[k]<<" ";
+				cout<<endl;
+				cout<<"Scale: ";
+				pos = item->getScale();
+				for(int k = 0;k<pos.size();k++)cout<<pos[k]<<" ";
+				cout<<endl<<endl<<endl;
+				*/
+		
+	}
+
+
 	//  helper function to parse string
 	int Modeler::nextInt(string s){
 		return stoi(s);
@@ -66,6 +175,120 @@ namespace modl
 				queue.push_back(item);
 				//print(item);
 	}
+
+
+	void Modeler::comp(vector<string> childSymbolNames,Symbol* Parent){
+		//create Children
+
+		for(int i = 0;i<childSymbolNames.size();i++){		
+
+			Symbol* item = new Symbol(childSymbolNames[i]);		
+			Vector3 parentPos = Parent->getPosition();
+			Vector3 parentScale = Parent->getScale();
+
+			if(childSymbolNames[i].compare("front")==0){				
+				item->setPosition(parentPos.x, parentPos.y, parentPos.z);
+				item->setScale(parentScale.x, parentScale.y,0.0);
+			}
+			if(childSymbolNames[i].compare("left_facade")==0){
+				item->setPosition(parentPos.x, parentPos.y, parentPos.z);
+				item->setScale(0, parentScale.y,parentScale.z);
+			}
+			if(childSymbolNames[i].compare("right_facade")==0){
+				item->setPosition(parentPos.x+parentScale.x, parentPos.y, parentPos.z);
+				item->setScale(0, parentScale.y,parentScale.z);
+			}
+			if(childSymbolNames[i].compare("back")==0){
+				item->setPosition(parentPos.x, parentPos.y, parentPos.z+parentScale.z);
+				item->setScale(parentScale.x, parentScale.y,0.0);
+			}
+			if(childSymbolNames[i].compare("bottom")==0){
+				item->setPosition(parentPos.x, parentPos.y, parentPos.z);
+				item->setScale(parentScale.x,0.0 ,parentScale.z);
+			}
+			if(childSymbolNames[i].compare("top")==0){
+				item->setPosition(parentPos.x, parentPos.y+parentScale.y, parentPos.z);
+				item->setScale(parentScale.x,0.0 ,parentScale.z);
+			}	
+				
+			Parent->Children.push_back(item);	
+			queue.push_back(item);
+			//print(item);
+		
+			
+		}
+		
+	}
+
+
+	void Modeler::insertMap1(int id, string symbolName){
+		map<string, vector<int>>::iterator it;
+		it = map1.find(symbolName);
+		if(it!=map1.end()){			
+			it->second.push_back(id);
+		}else{
+			vector<int>v_map1;
+			v_map1.push_back(id);
+			map1.insert(pair<string,vector<int>>(symbolName,v_map1));
+		}
+	}
+	
+	void Modeler::subDiv(vector<string> parameterValues, vector<string> childSymbolNames, Symbol* Parent){
+		//regard to which coordination divide
+		//with which sizes
+		//children names
+		//scales and positions of children
+		
+
+		string axis = parameterValues[0];		
+		Vector3 parentPos = Parent->getPosition();
+		Vector3 parentScale = Parent->getScale();
+		if(axis.compare("\"X\"")==0){	
+
+		double prepos= parentPos.x;	
+
+		for(int i = 0;i<childSymbolNames.size();i++){				
+			Symbol* item = new Symbol(childSymbolNames[i]);				
+			item->setPosition(prepos,parentPos.y, parentPos.z);
+			item->setScale(nextDouble(parameterValues[i+1]), parentScale.y, parentScale.z);
+			prepos+=nextDouble(parameterValues[i+1]);
+			
+			Parent->Children.push_back(item);	
+			queue.push_back(item);
+
+		}
+		}else if(axis.compare("\"Y\"")==0){		
+			double prepos = parentPos.y;
+			
+			for(int i = 0;i<childSymbolNames.size();i++){
+				Symbol* item = new Symbol(childSymbolNames[i]);					
+				item->setPosition(parentPos.x,prepos, parentPos.z);
+				item->setScale(parentScale.x, nextDouble(parameterValues[i+1]), parentScale.z);
+			
+				prepos+=nextDouble(parameterValues[i+1]);
+				
+				Parent->Children.push_back(item);	
+				queue.push_back(item);
+				
+			}
+		}else{
+			double prepos = parentPos.z;	
+
+			for(int i = 0;i<childSymbolNames.size();i++){		
+				Symbol* item = new Symbol(childSymbolNames[i]);		
+				item->setPosition(parentPos.x, parentPos.y,prepos);
+				item->setScale(parentScale.x, parentScale.y, nextDouble(parameterValues[i+1]));		
+				prepos+=nextDouble(parameterValues[i+1]);
+
+				
+				Parent->Children.push_back(item);		
+				queue.push_back(item);
+				
+			}
+		}
+	}
+
+
 
 	void Modeler::repeat(vector<string> parameterValues, vector<string> childSymbolNames, Symbol* Parent){
 	 //regard to which coordinate
